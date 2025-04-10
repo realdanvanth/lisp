ee052675d7dcfa6f77a1eb6826e3bbdcad4a1bec
realdanvanth <realdanvanth@gmail.com>
2025-04-10 14:28:02 +0530

    Update function.java

--- function.java
+++ function.java
@@ -6,23 +6,121 @@ class function
   {
     this.varMap = varMap;
   }
+  public String strip(String s)
+  {
+    if(s.charAt(0)=='('&&s.charAt(s.length()-1)==')')
+    {
+      return s.substring(1,s.length()-1);
+    }
+    return s;
+  }
   String parseString(String x)
   {
-    /*if(x.charAt(0)=='(')
+    if(x.charAt(0)=='(')
     {
-      //exec.atom...();
-    }*/
-    if(x.charAt(0)!='\'')
+      atom subatom = new atom(strip(x));
+      return subatom.exec();
+    }
+    else if(x.charAt(0)!='\'')
     {
-      return varMap.get(x).substring(1,varMap.get(x).length()-1);
+      return varMap.get(x);
     } 
     else
-    {
-      return x;
+    { 
+      return x.substring(1,x.length()-1);
     }
   }
-  void print(String x)
+  public String add(String x)
   {
-    System.out.println(parseString(x));
+    ArrayList<String> parameters = depthSplit(x,' ');
+    System.out.println("Parameters :"+parameters);
+    if(parameters.size()!=2)
+    {
+      return "Parameters Error";
+    }
+    else
+    {
+      return Integer.toString(Integer.parseInt(get(parameters.get(0)))+Integer.parseInt(get(parameters.get(1))));
+    }
+  }
+  public String mul(String x)
+  {
+    ArrayList<String> parameters = depthSplit(x,' ');
+    System.out.println("Parameters :"+parameters);
+    if(parameters.size()!=2)
+    {
+      return "Parameters Error";
+    }
+    else
+    {
+      return Integer.toString(Integer.parseInt(get(parameters.get(0)))*Integer.parseInt(get(parameters.get(1))));
+    }
+  }
+  public String sub(String x)
+  {
+    ArrayList<String> parameters = depthSplit(x,' ');
+    System.out.println("Parameters :"+parameters);
+    if(parameters.size()!=2)
+    {
+      return "Parameters Error";
+    }
+    else
+    {
+      return Integer.toString(Integer.parseInt(get(parameters.get(0)))-Integer.parseInt(get(parameters.get(1))));
+    }
+  }
+  public String mod(String x)
+  {
+    ArrayList<String> parameters = depthSplit(x,' ');
+    System.out.println("Parameters :"+parameters);
+    if(parameters.size()!=2)
+    {
+      return "Parameters Error";
+    }
+    else
+    {
+      return Integer.toString(Integer.parseInt(get(parameters.get(0)))/Integer.parseInt(get(parameters.get(1))));
+    }
+  }
+
+  public void terminate(int x)
+  {
+    System.out.println("Invalid Function Syntax ("+x+") : Exiting");
+    System.exit(0);
+  }
+  String get(String x)
+  {
+    return parseString(x);
+  }
+  ArrayList<String> depthSplit(String code,char a)//give with the spaces
+  {
+    int depth = 0;
+    int start = 0;
+    ArrayList<String> entries = new ArrayList<>();
+     for(int i = 0;i<code.length();i++)
+    {
+      if(code.charAt(i)=='(')
+        depth++;
+      else if(code.charAt(i)==')')
+        depth--;
+      if (code.charAt(i) == a && depth == 0)
+      {
+        if(start>=i)
+        {
+          terminate(1);
+        }
+        entries.add(code.substring(start,i));
+        start = i + 1;
+      }
+    }
+    entries.add(code.substring(start));
+    return entries;
+  }
+
+  public static void main(String args[]) 
+  {
+    function a= new function(null);
+    System.out.println(a.depthSplit("(x=2;get x) 3",' '));
+    System.out.println(a.add("'2' '3'"));
   }
 }
